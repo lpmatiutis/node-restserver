66d5e2488897042422e0791472792bd7f25dbb91
lpmatiutis <lpthemaster@hotmail.com>
2019-01-15 11:43:03 -0300

    restserver -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,23 @@
 //Puerto
 //========
 process.env.PORT = process.env.PORT || 3000;
+
+//========
+//Entorno
+//========
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//========
+//Base de datos
+//========
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:a.123456@ds157654.mlab.com:57654/cafe';
+}
+
+process.env.URLDB = urlDB;
